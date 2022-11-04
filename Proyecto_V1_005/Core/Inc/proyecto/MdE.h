e635de1f6e4883f021f675cde423630abdf89a7b
Tobias <guerrero@frba.utn.edu.ar>
2022-11-04 18:20:16 -0300

    Proyecto V1.005

---
+++ Proyecto_V1_005/Core/Inc/proyecto/MdE.h
@@ -0,0 +1,12 @@
+#ifndef MdE_H_
+#define MdE_H_
+
+/************************************************************************************************************
+ *** PROTOTIPO DE FUNCIONES
+ ************************************************************************************************************/
+void MdE_Principal ( void *p );
+void MdE_Preparar ( void *p );
+void Tarea_Errores( void *p );
+void Tarea_Botones( void *p );
+
+#endif
