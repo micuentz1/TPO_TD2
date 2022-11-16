18ea47be326c3857c19b743003a542bec808a561
JuanRonchetti <jronchetti@frba.utn.edu.ar>
2022-11-16 16:56:21 -0300

    LCD modificado

---
+++ Prueba_LCD_v101/Core/Src/proyecto/funciones.c
@@ -0,0 +1,27 @@
+#include "funciones.h"
+#include "liquidcrystal_i2c.h"
+#include "OneWire.h"
+
+void InitAll(void) {
+
+	HAL_GPIO_WritePin(GREEN_PORT, GREEN_PIN, ON); // Sistema Prendido y funcionando
+
+	get_ROMid();
+
+	InitLCD();
+}
+
+void InitLCD()
+{
+	HD44780_Init(2);
+	HD44780_NoCursor();
+	HD44780_SetBacklight(100);
+	HD44780_Clear();
+	HD44780_SetCursor(0,0);
+	HD44780_PrintStr("Iniciando");
+	HD44780_SetCursor(0,1);
+	HD44780_PrintStr("Aguarde...");
+
+	HAL_Delay(500);
+}
+
